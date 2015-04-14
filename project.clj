0351d043700a3f614a7e78cc17cc71a26cce11e6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-04-14 22:38:03 +1000

    Removed old src subdir files

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject poltak-db "0.1.0-SNAPSHOT"
+(defproject PoltakDB "0.1.0-SNAPSHOT"
   :description "An experimental NoSQL database"
   :url "https://github.com/poltak/poltak-db"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot poltak-db.core
+  :main ^:skip-aot PoltakDB.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

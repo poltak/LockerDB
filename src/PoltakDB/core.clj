30a9dc1c760122fbe1b72e24c0f7fab46f6fc0c4
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-04-14 22:37:23 +1000

    Renamed src subdir

---
+++ src/PoltakDB/core.clj
@@ -0,0 +1,9 @@
+(ns PoltakDB.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Welcome to poltak-db!"))
+
+
---
+++ src/PoltakDB/interface.clj
@@ -0,0 +1,7 @@
+(ns PoltakDB.interface
+  (:gen-class))
+
+(defn insert
+  "TODO: Check how graph DB actually works for insert")
+
+

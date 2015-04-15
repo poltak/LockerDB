d86d58dcae047fadc33fc650c833f7e3fe43dd0f
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-04-15 23:34:10 +1000

    Added bag and locker files

---
+++ src/PoltakDB/bag.clj
@@ -0,0 +1,9 @@
+(ns PoltakDB.bag
+  (:gen-class))
+
+(defn openBag
+  "Opens bag revealing its contents."
+  []
+  )
+
+
---
+++ src/PoltakDB/locker.clj
@@ -0,0 +1,29 @@
+(ns PoltakDB.locker
+  (:gen-class))
+
+"Initialises a new locker for bag storage."
+(def locker hash-map)
+
+(defn makeBag
+  "Makes a new empty bag and puts it in the locker."
+  [bagName]
+  (conj locker {:bagName hash-map})
+  (def currentBag :bagName))
+
+(defn getCurrentBag
+  "Returns the name of the currently used bag."
+  []
+  (currentBag))
+
+(defn switchBag
+  "Swaps current bag for specified bag.
+  TODO: handle nil"
+  [bagName]
+  (def currentBag (locker :bagName)))
+
+(defn showLockerContents
+  "Shows all bags currently in locker."
+  []
+  (locker))
+
+

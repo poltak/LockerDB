238fee852addd02ee0d62e3e3c22646eedf733ba
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-04-17 16:03:32 +1000

    Added bag functionality

--- src/LockerDB/bag.clj
+++ src/LockerDB/bag.clj
@@ -3,7 +3,23 @@
 
 (defn openBag
   "Opens bag revealing its contents."
-  []
-  )
+  [bag]
+  (bag))
+
+(defn addItem
+  "Adds a new item into the bag."
+  [bag itemKey itemVal]
+  (merge bag {:itemKey itemVal}))
+
+(defn removeItem
+  "Removes an existing item from the bag."
+  [bag itemKey]
+  (remove (= :itemKey) bag))
+
+(defn getItem
+  "Gets an existing item from the bag."
+  [bag itemKey]
+  (:itemKey bag))
+
 
 
